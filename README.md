5de21c831387e57940c62657fb26b1297f7c4699
Tim Force <tim.w.force@gmail.com>
2017-01-28 22:19:47 -0800

    misc. and production ready

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # quote_unquote_rails5
 
+Rails tutorial: finished chapters 1-7
+
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   get 'help', to: 'static_pages#help'
   # get 'home', to: 'static_pages#home'
   get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
 
   # root 'application#hello'
   root 'static_pages#home'
